65f45fe7d2affd8ba1f4106d4271bf95f3393fe0
Artsem Latuohuski <artsem.latuohuski@innowise-group.com>
2021-04-23 11:36:49 +0300

    add head-ui component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
+// outer
 import React from 'react';
 import './App.css';
+
+
+// local
+import Head_UI from "./components/head/Head_UI";
+
 
 function App() {
     return (
         <div>
-
+            <Head_UI/>
 
         </div>
     );
