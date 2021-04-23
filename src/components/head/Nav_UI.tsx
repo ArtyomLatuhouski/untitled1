5a59f968a793fc5b10a6b885bb50457ffb565a2a
Artsem Latuohuski <artsem.latuohuski@innowise-group.com>
2021-04-23 14:11:56 +0300

    clear cod in  header nav items

---
+++ src/components/head/Nav_UI.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+
+
+function a11yProps(index: any) {
+    return {
+        id: `simple-tab-${index}`,
+        'aria-controls': `simple-tabpanel-${index}`,
+    };
+}
+
+
+export default function SimpleTabs() {
+
+    const [value, setValue] = React.useState(0);
+
+    const handleChange = (event: React.ChangeEvent<{}>, newValue: number) => {
+        setValue(newValue);
+    };
+
+    return (
+
+        <Tabs value={value} onChange={handleChange} aria-label="simple tabs example">
+            <Tab label="Item One" {...a11yProps(0)} />
+            <Tab label="Item Two" {...a11yProps(1)} />
+            <Tab label="Item Three" {...a11yProps(2)} />
+        </Tabs>
+
+    );
+}
